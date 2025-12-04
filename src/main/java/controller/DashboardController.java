34558cda5952193e69d7de4678a425d82ed71595
malaka96 <madhubhashana655@gmail.com>
2025-12-04 23:03:54 +0530

    added basic structure of customer and product panes

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
 public class DashboardController {
+
+    @FXML
+    private Button customerBtn;
+
+    @FXML
+    private ScrollPane customerPane;
+
+    @FXML
+    private Button dashboardBtn;
+
+    @FXML
+    private Button productBtn;
+
+    @FXML
+    private ScrollPane productPane;
+
+    @FXML
+    void customerBtnAction(ActionEvent event) {
+        productPane.setVisible(false);
+        customerPane.setVisible(true);
+    }
+
+    @FXML
+    void productBtnAction(ActionEvent event) {
+        customerPane.setVisible(false);
+        productPane.setVisible(true);
+    }
+
 }
