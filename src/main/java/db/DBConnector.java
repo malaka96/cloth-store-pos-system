ad11f5e3f5d7b3a694943e347f569cde0f9e25c1
malaka96 <madhubhashana655@gmail.com>
2025-12-06 22:45:19 +0530

    updated dbcontroller

--- src/main/java/db/DBConnector.java
+++ src/main/java/db/DBConnector.java
@@ -4,13 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DBConnector {
+
+    private static final Dotenv dotenv = Dotenv.load();
 
     private Connection connection;
     private static DBConnector instance;
 
     private DBConnector() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothify","root","1234");
+        connection = DriverManager.getConnection(dotenv.get("DB_URL"),dotenv.get("DB_USER"),dotenv.get("DB_PASSWORD"));
     }
 
     public static DBConnector getInstance() throws SQLException {
